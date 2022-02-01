69947faa2f9043b8f488f3914b3d86d6938b2892
Mradul Gupta <93073227+mradul-gpt@users.noreply.github.com>
2022-02-01 12:26:47 +0530

    Event Page Made

--- src/Components/SubDomains/Event/EventPage.jsx
+++ src/Components/SubDomains/Event/EventPage.jsx
@@ -1,50 +1,37 @@
 import React from 'react';
 
-import second from './EventPage.css';
+import './EventPage.css';
+import DomainNavigation from '../DomainNavigation/DomainNavigation';
+import Footer from '../../Landing/Footer/Footer';
+import OldEvents from '../../Landing/Event/Event';
 
 
 class EventPage extends React.Component{
     render(){
         return (
-          <section id="EventPage">
+          <div className="EventPage">
+            <DomainNavigation />
             <h1>Our Exciting Events !</h1>
-            <div class="row">
-              <div class="col-lg-3">
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://discord.gg/k5pf4erckV"
-                >
-                  <img
-                    src="https://media.discordapp.net/attachments/923793151477563422/923794426659237939/9.png"
-                    alt=""
-                  />
-                </a>
-              </div>
-              <div class="col-lg-8 content1">
-                <h3>Code Club</h3>
-                <p>
-                  This club is more about getting like-minded students together
-                  to talk about and perhaps work on exciting coding projects
-                  together. We have generalized our club into 4 major coding
-                  domains:{" "}
-                  <b>
-                    Data and AI, Scripting, Development and Competitive
-                    Programming.
-                  </b>
-                </p>
-                <a
-                  target="_blank"
-                  rel="noreferrer"
-                  href="https://chat.whatsapp.com/FdJZLJoKwAk9KHvw1Tfuma"
-                >
-                  <button class="btn btn-light btn-outline-dark">
-                    Join Code Club
-                  </button>
-                </a>
+            <div className="latest-event">
+              <img src="https://cdn.discordapp.com/attachments/923793151477563422/937936822078701608/Space_Club_LinkedIn_Event_Poster.png" className='latest-img' alt="" />
+              <div className='row latest-content'>
+                <div className="col-lg-5 latest-sub-content">
+                  <p>✨ Civilization is like a thin layer of ice on a deep ocean of opportunities✨</p>
+                  <p>And if you wish to explore that ocean, we at 🛰️ CSCult's Space Club have something for you</p>
+                  <p className='event-name'>💫 Scales of Civilization💫</p>
+                  <p>Transcending from a type 0 to a type 1 civilization </p>
+                  <p>A session on the future of spaceflight and becoming a multiplanetary species 🇲🇭</p>
+                </div>
+                <div className="col-lg-5 latest-join-content">
+                  <h4>JOIN US !</h4><br />
+                  <p>🗓️ Date : 5 February 2022 <br />⏰ Time : 11:00 AM <br />📍Venue : Zoom</p>
+                  <a target='_blank' rel='noreferrer' href="https://us05web.zoom.us/j/87048877691?pwd=SitaYXBtbkpFZGx2T0lYUEpOVmlUdz09"><button className='btn btn-lg btn-primary btn-outline-light'>Join Event</button></a>
+                </div>
               </div>
             </div>
-          </section>
+            <OldEvents />
+            <Footer />
+          </div>
         );
     }
 }
